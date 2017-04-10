34f3c0d1bdc6a61fba28d141be412b7a2f4608d3
mireyasantiago <mireyasantiago105@gmail.com>
2017-04-10 01:19:08 -0500

    incompleto

---
+++ ejercicio-01.js
@@ -0,0 +1,19 @@
+function isValidCard(creditCardNumber) {
+    // Escribe tu código aquí
+    if(typeof(creditCardNumber)!== 'number'){
+     return "Error de dato"
+ }
+   if(creditCardNumber=='undefined'){
+     return "Ingresa un dato"
+}
+   if (creditCardNumber.toString().length!==16){
+      return "Faltan numeros";
+}
+}
+
+}
+
+// NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
+if (typeof exports !== 'undefined') {
+    exports.isValidCard = isValidCard;
+}
