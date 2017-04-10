5d9824f7495d348c5e3d497a1b1c78067c745f83
mireyasantiago <mireyasantiago105@gmail.com>
2017-04-10 08:57:38 -0500

    version

--- ejercicio-01.js
+++ ejercicio-01.js
@@ -1,17 +1,17 @@
 function isValidCard(creditCardNumber) {
     // Escribe tu código aquí
-    if(typeof(creditCardNumber)!== 'number'){
-     return "Error de dato"
- }
-   if(creditCardNumber=='undefined'){
-     return "Ingresa un dato"
-}
-   if (creditCardNumber.toString().length!==16){
+    if(typeof(creditCardNumber)!= 'number'){
+     return "Error de dato";
+   }
+    if(creditCardNumber == 'undefined'){
+     return "Ingresa un dato";
+   }
+    if (creditCardNumber.toString().length !== 16){
       return "Faltan numeros";
-}
-}
+   }
+ }
 
-}
+
 
 // NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
 if (typeof exports !== 'undefined') {
